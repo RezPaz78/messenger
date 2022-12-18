53ea40dda13788b78235e92ec4ccdbf55efdc3ae
Reza Pazan <rezapazan@rocketmail.com>
2022-12-18 19:17:58 +0330

    init tailwindcss

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 export default function Home() {
 	return (
 		<>
@@ -9,7 +8,7 @@ export default function Home() {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<main className={styles.main}>
+			<main>
 				<h2>Welcome to the chat app!</h2>
 			</main>
 		</>
